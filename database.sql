0d6c50ac7fd0e26270c7ea5269e50508b522ee21
Luis Manrique <luisito_099@hotmail.com>
2023-08-12 15:19:43 -0500

    add library jwt

--- database.sql
+++ database.sql
@@ -12,7 +12,7 @@ password        varchar(255) NOT NULL,
 description     text,
 image           varchar(255),
 created_at      datetime DEFAULT NULL,
-update_at       datetime DEFAULT NULL,
+updated_at       datetime DEFAULT NULL,
 remember_token  varchar(255),
 CONSTRAINT pk_users PRIMARY KEY(id)
 )ENGINE=InnoDb;
